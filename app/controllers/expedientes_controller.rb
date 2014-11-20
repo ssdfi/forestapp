b84bad5b1f134cb5778f2d6382d9fd2961812984
Mauricio Miranda <maurimiranda@gmail.com>
2014-11-20 20:25:05 -0300

    Trabajando en el filtro de fechas

--- app/controllers/expedientes_controller.rb
+++ app/controllers/expedientes_controller.rb
@@ -5,16 +5,18 @@ class ExpedientesController < ApplicationController
   # GET /expedientes.json
   def index
     @expediente = params[:expediente] ? Expediente.new(expediente_params) : Expediente.new({activo: true, incompleto: "false"})
-    
+
     @expedientes = Expediente.page params[:page]
 
     @expedientes = @expedientes.where("numero_interno ILIKE ?", "%#{@expediente.numero_interno}%") unless @expediente.numero_interno.blank?
     @expedientes = @expedientes.where("numero_expediente ILIKE ?", "%#{@expediente.numero_expediente}%") unless @expediente.numero_expediente.blank?
     @expedientes = @expedientes.where("numero_expediente IS #{'NOT' unless @expediente.incompleto} NULL") unless @expediente.incompleto.nil?
+    @expedientes = @expedientes.joins(:movimientos).where("movimientos.fecha_entrada >= ?", @expediente.fecha_desde) unless @expediente.fecha_desde.nil?
+    @expedientes = @expedientes.joins(:movimientos).where("movimientos.fecha_salida <= ?", @expediente.fecha_hasta) unless @expediente.fecha_hasta.nil?
     @expedientes = @expedientes.where(plurianual: @expediente.plurianual) unless @expediente.plurianual.nil?
     @expedientes = @expedientes.where(agrupado: @expediente.agrupado) unless @expediente.agrupado.nil?
     @expedientes = @expedientes.where(activo: @expediente.activo) unless @expediente.activo.nil?
-    
+
     @expedientes = @expedientes.order(updated_at: :desc)
 
     redirect_to @expedientes.first if @expedientes.count == 1
@@ -83,6 +85,6 @@ class ExpedientesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def expediente_params
-      params.require(:expediente).permit(:numero_interno, :numero_expediente, :tecnico, :plurianual, :agrupado, :activo, :incompleto)
+      params.require(:expediente).permit(:numero_interno, :numero_expediente, :tecnico, :plurianual, :agrupado, :activo, :incompleto, :fecha_desde, :fecha_hasta)
     end
 end
--- app/models/expediente.rb
+++ app/models/expediente.rb
@@ -2,10 +2,18 @@ class Expediente < ActiveRecord::Base
   has_many :movimientos
   has_and_belongs_to_many :titulares
 
-  attr_reader :incompleto, :zona, :departamento
+  attr_reader :incompleto, :fecha_desde, :fecha_hasta, :zona, :departamento
 
   def incompleto=(value)
     @incompleto = (value == "true") unless value.blank?
+  end
+
+  def fecha_desde=(value)
+    @fecha_desde = value unless value.blank?
+  end
+
+  def fecha_hasta=(value)
+    @fecha_hasta = value unless value.blank?
   end
 
   def zona
@@ -17,4 +25,13 @@ class Expediente < ActiveRecord::Base
     @departamento = @zona.departamentos.find_by_codigo(numero_interno[3..5]) unless @departamento
     @departamento
   end
+
+  def fecha_entrada
+    movimientos.order(:fecha_entrada).first.fecha_entrada
+  end
+
+  def fecha_salida
+    movimientos.order(:fecha_salida).last.fecha_salida
+  end
+
 end
