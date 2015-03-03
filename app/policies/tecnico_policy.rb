5d537f6b0030540469a1087bb88f681a2e72ef92
Mauricio Miranda <maurimiranda@gmail.com>
2015-03-03 13:29:33 -0300

    Arreglar problemas de permisos

--- app/policies/tecnico_policy.rb
+++ app/policies/tecnico_policy.rb
@@ -1,4 +1,24 @@
 class TecnicoPolicy < ApplicationPolicy
+  def create?
+    user.admin?
+  end
+
+  def new?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def edit?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
   class Scope < Scope
     def resolve
       scope
